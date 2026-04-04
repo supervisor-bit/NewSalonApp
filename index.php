3d31ad66ea0c3620d6d177374f0e55d06bfeafdf
Martin Vítek <martinvitek@Mac.home>
2026-04-04 07:21:53 +0200

    Fix Javascript call for new visit button

--- index.php
+++ index.php
@@ -723,7 +723,7 @@
                 
                 <div style="flex:1;"></div>
                 
-                <button class="chip-btn chip-purple" onclick="novaNavsteva(<?= $active_client['id'] ?>)">
+                <button class="chip-btn chip-purple" onclick="ukazNovaNavsteva()">
                     <svg width="14" height="14" fill="none" stroke="currentColor" stroke-width="2.5" viewBox="0 0 24 24"><line x1="12" y1="5" x2="12" y2="19"/><line x1="5" y1="12" x2="19" y2="12"/></svg>
                     Nová návštěva
                 </button>
